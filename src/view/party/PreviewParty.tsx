8872c5f4aa20ced90daa45eff87bb19187d5f366
sein0112 <tpdls973@naver.com>
2023-10-17 18:44:16 +0900

    feat: 파티확인 파일 생성

---
+++ src/view/party/PreviewParty.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+// import "./scss/style.scss"
+import '../../assets/css/preview.css'
+
+function PreviewParty() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <div className="element">
+                <div className="div">
+                    <p className="p">
+          <span className="text-wrapper">
+            생성된 파티를
+            <br />
+          </span>
+                        <span className="span">확인</span>
+                        <span className="text-wrapper">해보세요.</span>
+                    </p>
+                    <div className="overlap">
+                        <div className="rectangle" />
+                        <img className="vector" alt="Vector" src="vector-5.svg" />
+                        <div className="group">
+                            <div className="overlap-group">
+                                <div className="text-wrapper-2">hurry!</div>
+                            </div>
+                        </div>
+                        <div className="text-wrapper-3">드림 슬레이브</div>
+                        <div className="text-wrapper-4">
+                            마감시간
+                            <br />
+                            파티원
+                        </div>
+                        <div className="text-wrapper-5">
+                            2023/09/06 12:13
+                            <br />
+                            4명
+                        </div>
+                        <p className="text-wrapper-6">
+                            김진미 프로
+                            <br />
+                            김세인 프로
+                            <br />
+                            조도은 프로
+                            <br />
+                            정민재 프로
+                        </p>
+                        <img className="img" alt="Vector" src="image.svg" />
+                        <img className="vector-2" alt="Vector" src="vector-2.svg" />
+                        <img className="vector-3" alt="Vector" src="vector-3.svg" />
+                        <img className="vector-4" alt="Vector" src="vector-4.svg" />
+                        <div className="text-wrapper-7">메가커피</div>
+                        <img className="line" alt="Line" src="line-2.svg" />
+                        <img className="line-2" alt="Line" src="line-3.svg" />
+                    </div>
+                    <div className="overlap-wrapper">
+                        <div className="div-wrapper">
+                            <div className="text-wrapper-8">파티생성</div>
+                        </div>
+                    </div>
+                    <div className="overlap-group-wrapper">
+                        <div className="overlap-2">
+                            <div className="text-wrapper-9">이전으로</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
