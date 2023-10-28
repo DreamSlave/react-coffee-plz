1a3bb78f6e71a9a90b96e118b85e52e1ea7c8422
jinmi <jinmikim88@gmail.com>
2023-10-28 14:48:30 +0900

    feat : party 퍼블리싱 추가

---
+++ src/views/party/ConfirmParty.tsx
@@ -0,0 +1,51 @@
+// import React from "react";
+// import "./style.css";
+import '../../assets/css/confirmParty.css'
+
+const ConfirmParty = () => {
+  return (
+    <div className="element">
+      <div className="div">
+        <div className="btn-group">
+          <div className="large-btn-b">
+            <div className="overlap-group">
+              <div className="text-wrapper">추가 파티 생성</div>
+            </div>
+          </div>
+        </div>
+        <div className="order-link-area">
+          <div className="overlap">
+            <div className="order-link-btn">
+              <div className="group">
+                <img className="vector" alt="Vector" src="image.svg" />
+                <div className="text-wrapper-2">주문하러가기</div>
+              </div>
+            </div>
+            <div className="text-wrapper-3">
+              파티가
+              <br />
+              생셩되었습니다!
+              <br />
+              링크를 공유하세요.
+            </div>
+            <div className="cffe-plz">
+              <div className="div-wrapper">
+                <div className="text-wrapper-4">hurry!</div>
+              </div>
+            </div>
+            <div className="order-copylink">
+              <div className="overlap-2">
+                <div className="text-wrapper-5">hts://www.figma.com/file/R6Ojisdffs...</div>
+                <img className="copy-ic" alt="Copy ic" src="copy-ic.png" />
+              </div>
+              <img className="line" alt="Line" src="line-1.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default ConfirmParty;
---
+++ src/views/party/SaveParty.tsx
@@ -0,0 +1,66 @@
+// import React from "react";
+// import "./style.css";
+import '../../assets/css/saveParty.css'
+
+const SaveParty = () => {
+  return (
+    <div className="element">
+      <div className="div">
+        <p className="p">
+          <span className="text-wrapper">
+            본격적으로
+            <br />
+          </span>
+          <span className="span">파티를 생성</span>
+          <span className="text-wrapper">합니다.</span>
+        </p>
+        <div className="form-input">
+          <div className="overlap-group">
+            <img className="line" alt="Line" src="line-1.svg" />
+            <div className="text-wrapper-2">드림 슬레이브</div>
+          </div>
+          <div className="text-wrapper-3">파티명</div>
+        </div>
+        <div className="form-select">
+          <div className="overlap-group">
+            <img className="line" alt="Line" src="image.svg" />
+            <div className="text-wrapper-2">선택안함</div>
+            <img className="vector" alt="Vector" src="vector.svg" />
+          </div>
+          <div className="text-wrapper-3">카페선택</div>
+        </div>
+        <div className="formbox">
+          <div className="form-calendar">
+            <div className="overlap-group-2">
+              <img className="line" alt="Line" src="line-1-2.svg" />
+              <div className="text-wrapper-2">2023/09/06</div>
+              <img className="img" alt="Calendar ic" src="calendar-ic.png" />
+            </div>
+          </div>
+          <div className="form-time">
+            <div className="overlap-group-2">
+              <img className="line" alt="Line" src="line-1-3.svg" />
+              <div className="text-wrapper-2">12:13</div>
+              <img className="img" alt="Clock ic" src="clock-ic.png" />
+            </div>
+          </div>
+          <div className="text-wrapper-3">마감설정</div>
+        </div>
+        <div className="btn-group">
+          <div className="large-btn">
+            <div className="div-wrapper">
+              <div className="text-wrapper-4">미리보기</div>
+            </div>
+          </div>
+          <div className="large-btn-nd">
+            <div className="overlap">
+              <div className="text-wrapper-5">이전으로</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SaveParty;
