2a3e085e80d72d24239451cb3fd5c533664913c4
dohvely <chode8703@gmail.com>
2023-11-14 20:08:26 +0900

    💄 style - entrance 컴포넌트 style 적용(임시)

--- src/views/Entrance.tsx
+++ src/views/Entrance.tsx
@@ -1,23 +1,29 @@
-import '../assets/css/entrance.css'
+// import React from "react";
+import "../assets/temp-entrance/entrance.css";
+import hurryImg from '../assets/temp-entrance/entrance1.png'
+import logoImg from '../assets/temp-entrance/entrance2.png'
 
 const Entrance = () => {
   return (
     <div className="element">
-      <div className="div">
-        <div className="text-wrapper">
-          커피주문
-          <br />
-          올려주세요
-        </div>
-        <div className="cffe-plz">
-          <div className="overlap-group">
-            <div className="text-wrapper-2">hurry!</div>
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="contents">
+            <div className="text-wrapper">
+              커피주문
+              <br />
+              올려주세요
+            </div>
+            <div className="cffe-plz">
+              <img className="hurry" alt="Hurry" src={hurryImg} />
+            </div>
+            <div className="large-btn">
+              <div className="overlap-group">
+                <div className="div" style={{cursor: 'pointer'}}>파티생성하기</div>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="large-btn">
-          <div className="overlap">
-            <div className="text-wrapper-3">파티생성하기</div>
-          </div>
+          <img className="img" alt="Element" src={logoImg} />
         </div>
       </div>
     </div>
