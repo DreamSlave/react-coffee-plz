8fdf89ff006bb305eefc37c5299b13367a390ada
jinmi <jinmikim88@gmail.com>
2023-11-18 16:39:21 +0900

    :heavy_plus_sign: feat : 라우터추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import HeaderLayout from './layout/HeaderLayout';
 import NoHeaderLayout from './layout/NoHeaderLayout'
 import Entrance from './views/Entrance';
+import SelectPartyMember from './views/party/SelectPartyMember';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
               <Routes>
                 <Route path="/entrance" element={<Entrance />} />
                 {/* 다른 페이지 라우트도 여기에 추가할 수 있음 */}
+                <Route path="/party/select" element={<SelectPartyMember />} />
               </Routes>
             </NoHeaderLayout>
           } />
