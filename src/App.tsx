31df62254a36e8db53b848955092b14cc384f8e4
jinmi <jinmikim88@gmail.com>
2023-11-28 18:17:35 +0900

    :zap: feat : 라우터방식 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import "./assets/css/style.scss"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import HeaderLayout from './layout/HeaderLayout';
+import HeaderLayout from './layout/HeaderLayout';
 import NoHeaderLayout from './layout/NoHeaderLayout'
 import Entrance from './views/Entrance';
 import SelectPartyMember from './views/party/SelectPartyMember';
@@ -16,10 +16,19 @@ function App() {
             <NoHeaderLayout>
               <Routes>
                 <Route path="/entrance" element={<Entrance />} />
-                {/* 다른 페이지 라우트도 여기에 추가할 수 있음 */}
-                <Route path="/party/select" element={<SelectPartyMember />} />
               </Routes>
             </NoHeaderLayout>
+          } />
+      </Routes>
+      <Routes>
+        <Route
+          path="/party/*"
+          element={
+            <HeaderLayout>
+              <Routes>
+                <Route path="/select" element={<SelectPartyMember />} />
+              </Routes>
+            </HeaderLayout>
           } />
       </Routes>
     </Router>
