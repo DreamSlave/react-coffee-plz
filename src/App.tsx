cbeb213d508a7f8279f281c18d744a7f5df6176c
dohvely <chode8703@gmail.com>
2023-12-05 19:28:49 +0900

    🔥 remove - 불필요 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import "./assets/css/style.scss"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import HeaderLayout from './layout/HeaderLayout';
 import NoHeaderLayout from './layout/NoHeaderLayout'
 import Entrance from './views/Entrance';
 import SelectMenu from './views/order/SelectMenu';
