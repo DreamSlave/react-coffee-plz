b32b6a986bfb6ded5e263abd3308de2c29616f4a
jinmi <jinmikim88@gmail.com>
2024-01-09 20:44:21 +0900

    ➕ feat : 퍼블리싱 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,18 @@ const SaveParty = lazy(() => import('./views/party/SaveParty'))
 const OrderDetail = lazy(() => import('./views/order/OrderDetail'))
 const OrderPopup = lazy(() => import('./views/order/OrderPopup'))
 const OrderMember = lazy(() => import('./views/order/OrderMember'))
+const PreviewParty = lazy(() => import('./views/party/PreviewParty'))
+const ConfirmParty = lazy(() => import('./views/party/ConfirmParty'))
+
+const DesignEntrance = lazy(() => import('./views_design/Entrance'))
+const DesignSelectMenu = lazy(() => import('./views_design/order/SelectMenu'))
+const DesignSelectPartyMember = lazy(() => import('./views_design/party/SelectPartyMember'))
+const DesignSaveParty = lazy(() => import('./views_design/party/SaveParty'))
+const DesignOrderDetail = lazy(() => import('./views_design/order/OrderDetail'))
+const DesignOrderPopup = lazy(() => import('./views_design/order/OrderPopup'))
+const DesignOrderMember = lazy(() => import('./views_design/order/OrderMember'))
+const DesignPreviewParty = lazy(() => import('./views_design/party/PreviewParty'))
+const DesignConfirmParty = lazy(() => import('./views_design/party/ConfirmParty'))
 
 function App() {
   return (
@@ -47,9 +59,48 @@ function App() {
                 <Routes>
                   <Route path="/select" element={<SelectPartyMember />} />
                   <Route path="/save" element={<SaveParty />} />
-                  
+                  <Route path="/preview" element={<PreviewParty />} />
+                  <Route path="/confirm" element={<ConfirmParty />} />
                 </Routes>
               </HeaderLayout>
+            } 
+          ></Route>
+        </Routes>
+        <Routes>
+          //퍼블리싱 화면
+        <Route
+          path="design/*"
+          element={
+            <NoHeaderLayout>
+              <Routes>
+                <Route path="/entrance" element={<DesignEntrance />} />
+              </Routes>
+            </NoHeaderLayout>
+          } />
+          <Route
+            path="design/order/*"
+            element={
+              <NoHeaderLayout>
+                <Routes>
+                  <Route path="/menu" element={<DesignSelectMenu/>}></Route>
+                  <Route path="/detail" element={<DesignOrderDetail />} />
+                  <Route path="/detail/popup" element={<DesignOrderPopup />} />
+                  <Route path="/detail/member" element={<DesignOrderMember />} />
+                </Routes>
+              </NoHeaderLayout>
+            }
+          ></Route>
+          <Route
+            path="design/party/*"
+            element={
+              <NoHeaderLayout>
+                <Routes>
+                  <Route path="/select" element={<DesignSelectPartyMember />} />
+                  <Route path="/save" element={<DesignSaveParty />} />
+                  <Route path="/preview" element={<DesignPreviewParty />} />
+                  <Route path="/confirm" element={<DesignConfirmParty />} />
+                </Routes>
+              </NoHeaderLayout>
             } 
           ></Route>
         </Routes>
