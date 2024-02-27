804cb67ff8285b5c6590f2082f4e48b8f37bb159
dohvely <chode8703@gmail.com>
2024-02-27 20:08:09 +0900

    ✨ feat - 주문완료화면 1. 주문자명 정보 표시 2. 주문 메뉴 표시(querystring으로 우선 작업) 3. 주문현황 화면으로 이동 버튼 기능 구현

--- src/views/order/CompleteMenu.tsx
+++ src/views/order/CompleteMenu.tsx
@@ -1,8 +1,49 @@
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const CompleteMenu = () => {
+  const orderer = {
+    partyNo: useSelector((state: RootState) => state.order.partyNo),
+    userId: useSelector((state: RootState) => state.order.userId),
+    name: useSelector((state: RootState) => state.order.name),
+    team: useSelector((state: RootState) => state.order.team),
+    rank: useSelector((state: RootState) => state.order.rank),
+  }
+  const { encodedMenuNm } = useParams<{ encodedMenuNm: string }>()
+  const [ menuNm, setMenuNm ] = useState<string>('')
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if(encodedMenuNm && encodedMenuNm !== '') {
+      setMenuNm(decodeURIComponent(encodedMenuNm))
+    }
+  }, [encodedMenuNm])
+
+
+  const goOrderDetail = () => {
+    navigate('/order/test')
+  }
+
   return (
-    <div className="element">
-      CompleteMenu!!!
+    <div id="order" className="element">
+      <div className="person-info bg_sub point">
+        {orderer.name} {orderer.rank}({orderer.team})
+      </div>
+      <div className="person-info bg_sub point">
+        {menuNm}
+      </div>
+      <div className='btn-area'>
+        <ul>
+          <li>
+            <div className="large-btn" onClick={goOrderDetail}>
+              확인
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
