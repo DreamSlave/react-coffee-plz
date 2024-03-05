efbf9d00b3e95dc13f482729892e26ac530db9f3
jinmi <jinmikim88@gmail.com>
2024-03-05 21:15:25 +0900

    💄 design : 퍼블리싱 적용

--- src/views/party/PreviewParty.tsx
+++ src/views/party/PreviewParty.tsx
@@ -1,70 +1,54 @@
 // import './App.css'
 // import "./scss/style.scss"
-import '../../assets/css/preview.css'
+// import '../../assets/css/all.css'
+import '../../assets_design/css/all.css'
+import '../../assets_design/css/style.scss'
 
 function PreviewParty() {
 
   return (
-    <>
-      <div className="element">
-        <div className="div">
-          <p className="p">
-            <span className="text-wrapper">
-              생성된 파티를
-              <br />
-            </span>
-            <span className="span">확인</span>
-            <span className="text-wrapper">해보세요.</span>
-          </p>
-          <div className="overlap">
-            <div className="rectangle" />
-            <img className="vector" alt="Vector" src="vector-5.svg" />
-            <div className="group">
-              <div className="overlap-group">
-                <div className="text-wrapper-2">hurry!</div>
+    <div id='party' className="element pbd50">
+        <h1>
+          생성된 파티를<br/><span className="point">확인</span>하세요.
+        </h1>
+
+        <div className="order-list-area mgt30">
+            <div className="clipimg">
+              <img src="/src/assets/img/clip_img.png" />
+            </div>
+            <div className="order-title">
+              <div className="text">
+                <div className="title">파티명</div>
+                <div className="cafenm point">카페이름</div>
+              </div>
+              <div className="image">
+                <img src="/src/assets/img/icon_hurry.png" />
               </div>
             </div>
-            <div className="text-wrapper-3">드림 슬레이브</div>
-            <div className="text-wrapper-4">
-              마감시간
-              <br />
-              파티원
+            <div className="order-time">
+              <span className="mgr5"><b>마감시간</b></span> 2023/09/06 12:22
             </div>
-            <div className="text-wrapper-5">
-              2023/09/06 12:13
-              <br />
-              4명
+            
+            <div className="order-list-detail">
+              <div className="title"><b>파티원 <span className="point">4</span>명</b></div>
+              <div className="list">
+                <ul>
+                  <li>1. 정민재 프로(UX디자인팀)</li>
+                  <li>2. 정민재 프로(UX디자인팀)</li>
+                  <li>3. 정민재 프로(UX디자인팀)</li>
+                  <li>4. 정민재 프로(UX디자인팀)</li>
+                </ul>
+              </div>
             </div>
-            <p className="text-wrapper-6">
-              김진미 프로
-              <br />
-              김세인 프로
-              <br />
-              조도은 프로
-              <br />
-              정민재 프로
-            </p>
-            <img className="img" alt="Vector" src="image.svg" />
-            <img className="vector-2" alt="Vector" src="vector-2.svg" />
-            <img className="vector-3" alt="Vector" src="vector-3.svg" />
-            <img className="vector-4" alt="Vector" src="vector-4.svg" />
-            <div className="text-wrapper-7">메가커피</div>
-            <img className="line" alt="Line" src="line-2.svg" />
-            <img className="line-2" alt="Line" src="line-3.svg" />
-          </div>
-          <div className="overlap-wrapper">
-            <div className="div-wrapper">
-              <div className="text-wrapper-8">파티생성</div>
-            </div>
-          </div>
-          <div className="overlap-group-wrapper">
-            <div className="overlap-2">
-              <div className="text-wrapper-9">이전으로</div>
-            </div>
-          </div>
         </div>
+        
+        <footer id="footer" className="bg_white">
+          <div className="large-btn">
+              파티생성
+          </div>
+        </footer>
+      
       </div>
-    </>
   )
 }
 
