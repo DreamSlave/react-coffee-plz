10e88e26f6f9913dca2d9f37587cc88c2aa02653
sein <tpdls973@naver.com>
2024-03-12 19:22:19 +0900

    chore: base url 로컬용/빌드용으로 나눔

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/react-coffee-plz',
-  plugins: [react(), tsconfigPaths()],
+export default defineConfig(({mode})=> {
+  return {
+    base: mode === 'development' ? '/' :'/react-coffee-plz',
+    plugins: [react(), tsconfigPaths()],
+  }
 })
