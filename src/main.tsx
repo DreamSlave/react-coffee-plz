d98092953bba4f51b8318302dcace19832f02da3
sein <tpdls973@naver.com>
2024-05-14 19:48:23 +0900

    change: 새로고침시 redux 데이터 초기화 되는 현상 개선  - 리덕스 store 유지 라이브러리 설치

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {Provider} from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react';
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from "@/store/index";
 
-const store = configureStore({reducer : rootReducer});
+// const store = configureStore({reducer : rootReducer});
+import { store, persistor } from './store'; // Redux 스토어와 persistor 가져오기
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,24 @@
-import { combineReducers } from "redux";
 import counter from "./counter.ts";
 import order from "./order.ts";
 import party from "./party.ts";
+import {configureStore, combineReducers, createStore} from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import { PersistConfig } from 'redux-persist/es/types';
 
 const rootReducer = combineReducers({
   counter,
   order,
   party
 });
+const persistConfig: PersistConfig<any> = {
+  key: 'root',
+  storage,
+};
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = createStore(persistedReducer);
+export const persistor = persistStore(store);
 export default rootReducer;
 
 export type RootState = ReturnType<typeof rootReducer>;
