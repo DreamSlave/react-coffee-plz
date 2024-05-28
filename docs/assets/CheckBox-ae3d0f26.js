572440d30adb999b2756c61e6cff01f4e0b92471
sein <tpdls973@naver.com>
2024-05-28 19:33:33 +0900

    🚀 docs - Github pages 배포(임시)

---
+++ docs/assets/CheckBox-ae3d0f26.js
@@ -0,0 +1,1 @@
+import{r as a,j as o}from"./index-817c5466.js";const i=({children:r,checked:t,onChange:e})=>{const[n,s]=a.useState(t);a.useEffect(()=>{s(t)},[t]);const x=c=>{const h=c.target.checked;s(h),e&&e(c)};return o.jsxs("label",{children:[o.jsx("input",{type:"checkbox",checked:n,onChange:x}),r]})};export{i as C};
