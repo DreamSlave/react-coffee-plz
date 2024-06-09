b9b34dd5d9f4eeee264e100414e89b465c9fc3b2
dohvely <chode8703@gmail.com>
2024-06-09 17:33:12 +0900

    ♻️ refactor - Confirm 컴포넌트 반영 1. Header X 버튼 클릭시 Confirm 컴포넌트 구동 처리 2. Confirm 컴포넌트 "돌아가기"/"확인" 버튼 이벤트 구현

--- src/component/ConfirmForClose.tsx
+++ src/component/ConfirmForClose.tsx
@@ -1,5 +1,31 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import { RootState } from "@/store";
+import { useSelector} from "react-redux";
 
-function ConfirmForClose() {
+function ConfirmForClose({ onClose }) {
+  const userInfo = useSelector((state: RootState) => state.order);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const doConfirm = () => {
+
+    let goRouterPath = ''
+    switch (location.pathname){
+      case "/party/select":
+      case "/party/save":
+      case "/party/preview":
+        goRouterPath = "/entrance"
+        break
+      case "/order/member":
+      case "/order/menu":
+        goRouterPath = `/order/${userInfo.partyNo}`
+        break
+    }
+    navigate(goRouterPath);
+    onClose()
+  }
+
   return (
     <div id="popup">
       <div className="popup-area">
@@ -10,12 +36,12 @@ function ConfirmForClose() {
         <div className='btn-area'>
           <ul>
             <li>
-              <div className="large-btn bg_sub point">
+              <div className="large-btn bg_sub point" onClick={doConfirm}>
                 돌아가기
               </div>
             </li>
             <li>
-              <div className="large-btn">
+              <div className="large-btn" onClick={onClose}>
                 확인
               </div>
             </li>
--- src/layout/HeaderLayout.tsx
+++ src/layout/HeaderLayout.tsx
@@ -1,9 +1,11 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useSelector} from "react-redux";
 import { RootState } from "@/store";
 import backIcon from "@/assets/img/icon_back.png"
 import xIcon from "@/assets/img/icon_x.png"
+import ConfirmForClose from '@/component/ConfirmForClose'
+
 
 function HeaderLayout({ children }: { children: ReactNode}) {
   const userInfo = useSelector((state: RootState) => state.order);
@@ -11,37 +13,26 @@ function HeaderLayout({ children }: { children: ReactNode}) {
   const location = useLocation();
   const navigate = useNavigate();
   const isHiddenBackbtn = location.pathname.includes("/party/select") || location.pathname.includes("/order/member");
+  const [isConfirmOccur, setIsConfirmOccur] = useState(false)
 
   const onClickBack = () => {
     navigate(-1);
   };
 
   const onClickClose = () => {
-    const confirmResult = window.confirm('진행 중인 내용이 모두 사라집니다!\n종료하시겠습니까?')
-    if(!confirmResult) {
-      return
-    }
-
-    let goRouterPath = ''
-    switch (location.pathname){
-      case "/party/select":
-      case "/party/save":
-      case "/party/preview":
-        goRouterPath = "/entrance"
-        break
-      case "/order/member":
-      case "/order/menu":
-        goRouterPath = `/order/${userInfo.partyNo}`
-        break
-    }
-    navigate(goRouterPath);
+    handleConfirmOccur()
   };
+
+  const handleConfirmOccur = () => {
+    setIsConfirmOccur(!isConfirmOccur)
+  }
 
   return (
     <div>
       <div id="header">
         { !isHiddenBackbtn ? <img onClick={onClickBack} className="back_ic" src={backIcon} /> : ''}
         <img onClick={onClickClose} className="x_ic" src={xIcon} />
+        {isConfirmOccur && <ConfirmForClose onClose={handleConfirmOccur}></ConfirmForClose>}
       </div>
       {children}
     </div>
