9bc6f27450b7e36487d9e9ef534636592a4719e0
dohvely <chode8703@gmail.com>
2024-06-17 20:23:22 +0900

    ♻️ refactor - main.tsx <div id="global-layer"></div> 제거(주석상태)

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <GlobalUIProvider>
-            {/* <div id="global_layer"></div> */}
             <App />
           </GlobalUIProvider>
         </PersistGate>
