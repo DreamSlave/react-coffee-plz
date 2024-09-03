a6771442a6602428be92ad5e39421aa4e2deb13a
dohvely <chode8703@gmail.com>
2024-09-03 17:37:17 +0900

    📝 docs - githubpages 빌드 파일 배포

--- docs/assets/CheckBox-1482ba69.js
+++
@@ -1,1 +0,0 @@
-import{r as a,j as o}from"./index-7e2e7952.js";const i=({children:r,checked:t,onChange:e})=>{const[n,s]=a.useState(t);a.useEffect(()=>{s(t)},[t]);const x=c=>{const h=c.target.checked;s(h),e&&e(c)};return o.jsxs("label",{children:[o.jsx("input",{type:"checkbox",checked:n,onChange:x}),r]})};export{i as C};
