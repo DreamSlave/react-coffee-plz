12ba76b5c8ff54c8489a004deae0daf41f477751
dohvely <chode8703@gmail.com>
2024-09-03 16:37:38 +0900

    ♻️ change - API 도메인 DNS로 변경

--- src/api/api.config.ts
+++ src/api/api.config.ts
@@ -1,5 +1,6 @@
 
-const defaultDomain = 'http://13.209.0.85'
+const defaultDomain = 'https://order-coffee-api.etners.com'
+// const defaultDomain = 'http://13.209.0.85'
 export default {
   defaultDomain
 }
