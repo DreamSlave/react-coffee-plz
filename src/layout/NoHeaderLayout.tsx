b636a56faeb447e66c688a3fd64e9d919f419b57
jinmikim88 <jinmikim88@gmail.com>
2024-10-08 10:18:54 +0900

    🩹 fix : 뒤로가기 버튼 동작 추가

--- src/layout/NoHeaderLayout.tsx
+++ src/layout/NoHeaderLayout.tsx
@@ -1,6 +1,42 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 function HeaderLayout({ children }: { children: ReactNode}) {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleBackButton = () => {
+
+      // const handleBackButton = (event: PopStateEvent) => {
+        if(location.pathname.startsWith('/party/confirm')) {
+          navigate(`/entrance`, {replace: true})
+        } else {
+          navigate(-1)
+        }
+      }
+
+      window.addEventListener('popstate', handleBackButton)
+      return () => {
+        window.removeEventListener('popstate', handleBackButton)
+      }
+
+    }, [location, navigate])
+    //type2(react-router-dom v6 에서는 불가)
+    /* useEffect(() => {
+      const unlisten = navigate.listen(({action, location}) => {
+        if(action === 'POP' && location.pathname.startsWith('/order/complete')) {
+          navigate(`/order/${userInfo.partyNo}`, {replace: true})
+        }
+      })
+
+      return () => {
+        unlisten()
+      }
+    }, [navigate]) */
+
+
+
   return (
     <div>
       {children}
