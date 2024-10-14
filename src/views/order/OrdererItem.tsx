627760236ff4e484c0da83fc040bbbe9fe9c0141
comanjae <yo2comando@gmail.com>
2024-10-14 22:20:08 +0900

    design: 주문변경버튼 수정, 전화걸기버튼 수정, 셀렉박스 파란글씨 수정

--- src/views/order/OrdererItem.tsx
+++ src/views/order/OrdererItem.tsx
@@ -28,11 +28,15 @@ function Orderer({
         {userInfo.name} ({userInfo.team})
       </div>
           {isOrderComplete ?
-              <div className="done-label">주문완료</div>
-              : (!(/iPhone|iPad|Android|BlackBerry/i.test(navigator.userAgent)) || (<div style={{ background: '#ff5cc3', color: 'white', cursor: 'pointer' }}
-                     className="done-label"
+              <div className="done-label">주문변경</div>
+              : (!(/iPhone|iPad|Android|BlackBerry/i.test(navigator.userAgent)) || (<div 
+                     className="call-label"
                      onClick={() => document.location.href = `tel:${userInfo.telNo}`}
-              >전화걸기</div>))}
+              ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path fill-rule="evenodd" d="M15 3.75a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-1.5 0V5.56l-4.72 4.72a.75.75 0 1 1-1.06-1.06l4.72-4.72h-2.69a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
+              <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
+            </svg>
+            </div>))}
       </span>
     </>
   );
